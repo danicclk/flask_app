bc3366bd0975f001718c68b9804e8259dc1fd9b1
danicclk <31377550+danicclk@users.noreply.github.com>
2018-03-29 22:02:40 -0500

    routes basic edits 1

--- my_app.py
+++ my_app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello, World!</h1>'
 
+@app.route('/jobs')
+def jobs():
+	return '<h1>Carrer Tracks</h1>'
+
+@app.route('/resources')
+def resources():
+	return '<h1>Resources</h1>'
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
