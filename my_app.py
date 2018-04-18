10ef87cef611f01055f5b198d5a71c996942f151
danicclk <31377550+danicclk@users.noreply.github.com>
2018-04-18 07:21:58 -0500

    editing routes,
    rendering templates, minimal

--- my_app.py
+++ my_app.py
@@ -1,18 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+def index():
+    return render_template('welcome.html')
+    #  return '<h1>Hello World!</h1>'
 
-@app.route('/jobs')
-def jobs():
-	return '<h1>Carrer Tracks</h1>'
 
-@app.route('/resources')
-def resources():
-	return '<h1>Resources</h1>'
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+# @app.route('/')
+# @app.route('/jobs')
+# def jobs():
+#     return '<h1>Carrer Tracks</h1>'
+#
+#
+# @app.route('/resources')
+# def resources():
+#     return'<h1>Resources</h1>'
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
