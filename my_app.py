5b4eca7366fed8063b97bef0fc06d4a11862790d
danicclk <31377550+danicclk@users.noreply.github.com>
2018-05-01 19:42:38 -0500

    Created more routes
    Routes and templates for them.

--- my_app.py
+++ my_app.py
@@ -7,13 +7,31 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('welcome.html')
-    #  return '<h1>Hello World!</h1>'
+    return render_template('about.html')
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+
+
+@app.route('/concentrations')
+def concentrations():
+    return render_template('concentrations.html')
+
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 # @app.route('/')
 # @app.route('/jobs')
@@ -24,7 +42,3 @@ def page_not_found(e):
 # @app.route('/resources')
 # def resources():
 #     return'<h1>Resources</h1>'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
